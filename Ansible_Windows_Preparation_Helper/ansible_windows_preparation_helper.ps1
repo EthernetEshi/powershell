28621a1aaa6fdeefc9fd4cc4adda7e13fbdf5cde
EthernetEshi <imaneshragh03@gmail.com>
2026-04-16 09:14:25 +0200

    Refactor user creation and group assignment logic

--- Ansible_Windows_Preparation_Helper/ansible_windows_preparation_helper.ps1
+++ Ansible_Windows_Preparation_Helper/ansible_windows_preparation_helper.ps1
@@ -19,8 +19,9 @@ Write-Host "
 ██╔══██║██╔══╝  ██║     ██╔═══╝ ██╔══╝  ██╔══██╗                                       
 ██║  ██║███████╗███████╗██║     ███████╗██║  ██║                                       
 ╚═╝  ╚═╝╚══════╝╚══════╝╚═╝     ╚══════╝╚═╝  ╚═╝                                       
-                                                                                                                                                      
+                                                                                                                                                   
 "
+
 Write-Host "Enter your PowerShell remoting credentials: "
 $user = Read-Host "User"
 $pass = Read-Host "Password" -AsSecureString
@@ -29,33 +30,36 @@ $creds = [PSCredential]::new($user, $pass)
 $ServerListPath = Join-Path $PSScriptRoot 'remote_hosts.txt'
 # defining parameters
 $Servers = Get-Content $ServerListPath
+$AnsibleUserName = Read-Host "Enter the user name for the Ansible user"
 $AnsibleUserPassword = Read-Host "Enter password for the ansible user" -AsSecureString
 
 foreach ($Server in $Servers) {
-    Write-Host "[STEP 1] Connecting to $Server ..." -ForegroundColor Cyan
-
+    Write-Host "Connecting to $Server ... `n" -ForegroundColor Cyan
+    Write-Host "[STEP 1] Creating $AnsibleUserName and adding it to necessary user groups ...`n" -ForegroundColor Cyan
     try {
         Invoke-Command -ComputerName $Server -Credential $creds -Authentication Negotiate -ScriptBlock {
-            $CheckForUser = (Get-LocalUser).Name -contains 'ansible_user'
+            $CheckForUser = (Get-LocalUser).Name -contains $Using:AnsibleUserName
             if($CheckForUser -eq $false) {
                 try {
-                    New-LocalUser -Name 'ansible_user' -Password $Using:AnsibleUserPassword -Description 'Windows user for access via Ansible' -PasswordNeverExpires
-                    Add-LocalGroupMember -Group 'Administrators' -Member 'ansible_user' -ErrorAction Stop
-                    Write-Host "Added ansible_user to Administrators." -ForegroundColor Green
-                    Add-LocalGroupMember -Group 'OpenSSH Users' -Member 'ansible_user' -ErrorAction Stop
-                    Write-Host "Added ansible_user to OpenSSH Users." -ForegroundColor Green
-                    Add-LocalGroupMember -Group 'Remote Management Users' -Member 'ansible_user' -ErrorAction Stop
-                    Write-Host "Added ansible_user to Remote Management Users." -ForegroundColor Green
-                    # Remove-LocalGroupMember -Group 'Users' -Member 'ansible_user' -ErrorAction Stop
-                    # Write-Host "Removed ansible_user from Users." -ForegroundColor Green
+                    New-LocalUser -Name $Using:AnsibleUserName -Password $Using:AnsibleUserPassword -Description 'Windows user for access via Ansible'
+                    Add-LocalGroupMember -Group 'Administrators' -Member $Using:AnsibleUserName -ErrorAction Stop
+                    Write-Host "Added $Using:AnsibleUserName to Administrators." -ForegroundColor Green
+                    Add-LocalGroupMember -Group 'OpenSSH Users' -Member $Using:AnsibleUserName -ErrorAction Stop
+                    Write-Host "Added $Using:AnsibleUserName to OpenSSH Users." -ForegroundColor Green
+                    Add-LocalGroupMember -Group 'Remote Management Users' -Member $Using:AnsibleUserName -ErrorAction Stop
+                    Write-Host "Added $Using:AnsibleUserName to Remote Management Users." -ForegroundColor Green
                 } catch {
-                    Write-Host "Error while adding ansible_user to a group: $_" -ForegroundColor Red
+                    Write-Host "Error while adding $Using:AnsibleUserName to a group: $_" -ForegroundColor Red
                 }
+            } else {
+                Write-Host "$Using:AnsibleUserName already exists. Moving on... `n"
             }
+            Write-Host "[STEP 2] Disabling administrator token filter policy ..." -ForegroundColor Cyan
             try {
-                 New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System' -Name 'LocalAccountTokenFilterPolicy' -PropertyType DWord -Value 1 -Force
+                 New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System' -Name 'LocalAccountTokenFilterPolicy' -PropertyType DWord -Value 1 -Force | Out-Null
+                 Write-Host "Successfully added registry key!" -ForegroundColor Green
             } catch {
-                Write-Host "Error while removing administrator token filter policy" -ForegroundColor Red
+                Write-Host "Error while disabling administrator token filter policy" -ForegroundColor Red
             }   
            
                
